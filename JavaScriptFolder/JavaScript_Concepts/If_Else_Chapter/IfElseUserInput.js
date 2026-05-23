eff3f04c84ea185b330d3ba0b051ff203bf14d17
AbhishekShukla483 <AbhishekShukla483@users.noreply.github.com>
2026-05-23 15:31:47 +0530

    Add IfElseUserInput.js to If_Else_Chapter folder

---
+++ JavaScriptFolder/JavaScript_Concepts/If_Else_Chapter/IfElseUserInput.js
@@ -0,0 +1,13 @@
+let num = prompt("Enter a number:");
+num = Number(num);
+
+if (num %2 === 0)
+{
+    console.log(num + "is an even number");
+}
+else
+{
+    console.log(num + "is a odd number");
+}
+
+//Note: --> This program needs to run in console because this program is a user input program.
